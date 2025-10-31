094cdd53484823da7268bb3e306c7a4d9cd06817
waleedirfan2006-arch <waleedirfan2006@gmail.com>
2025-10-31 18:35:15 +0500

    Add files via upload

---
+++ nested.c
@@ -0,0 +1,12 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    for (int i=1;i<=6;i++)
+    {
+        for(int j=1;j<=i;j++)
+        printf("*");
+    
+    printf("\n");
+    }
+}
---
+++ pyramid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = i; j < n; j++)
+            printf(" ");     // print spaces
+        for (j = 1; j <= (2 * i - 1); j++)
+            printf("*");     // print stars
+        printf("\n");
+    }
+    return 0;
+}
+
