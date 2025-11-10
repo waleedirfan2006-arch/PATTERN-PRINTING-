95c52528a985f8142b50453890fd013901855ecc
Waleed Irfan <waleedirfan2006@gmail.com>
2025-11-10 19:55:55 +0500

    Add files via upload

---
+++ right angle triangle.c
@@ -0,0 +1,26 @@
+/* to print 
+*
+**
+***
+****
+*****
+*/
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for (i=1;i<=5;i++)
+    {
+        for(j=i;j<5;j++)
+        {
+            printf("  ");
+
+        }
+        for(int k=1;k<=i;k++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
